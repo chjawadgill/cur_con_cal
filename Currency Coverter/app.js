645cd9680b4d4e5871e54a4d65aa016a7c017de9
Jawad Liaqat <chjawadgill@gmail.com>
2026-02-11 14:54:29 +0000

    API not found

--- Currency Coverter/app.js
+++ Currency Coverter/app.js
@@ -1,2 +1,10 @@
-const baseUrl = "https://v6.exchangerate-api.com/v6/fda0a5513519cd6f19c93d54/latest/pkr";
-const dropdown = document.getquerySelectorAll(".dropdown select");
+const BASE_URL =  "https://v6.exchangerate-api.com/v6/fda0a5513519cd6f19c93d54/latest/USD";
+
+const getCurrencyCodes = async () =>{
+    console.log ('Getting Currency Codes');
+    getdata = await fetch(BASE_URL);
+    data = await getdata.json();
+    cur = data.conversion_rates.PKR;
+    bel = 5*cur
+    console.log(bel);
+}
